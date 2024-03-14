e426b6c9d0dea1b0f449ca0033c1d45bcc4f9346
Proxify <brad.will24@gmail.com>
2024-03-14 18:33:11 -0500

    Fixed some exceptions that'd occur in the Smithing class

--- src/main/java/net/botwithus/api/game/skills/smithing/Smithing.java
+++ src/main/java/net/botwithus/api/game/skills/smithing/Smithing.java
@@ -7,6 +7,7 @@ import net.botwithus.rs3.game.js5.types.StructType;
 import net.botwithus.rs3.game.js5.types.configs.ConfigManager;
 import net.botwithus.rs3.game.skills.Skills;
 import net.botwithus.rs3.game.vars.VarManager;
+import net.botwithus.rs3.script.ScriptConsole;
 
 /**
  * Exposes information related to the players inventory in regard to smithing
@@ -86,11 +87,17 @@ public final class Smithing {
     public static int getMaximumHeat(int slot) {
         EnumType etype = ConfigManager.getEnumType(15095);
         int key = VarManager.getInvVarbit(93, slot, 43222);
+        if (etype == null) {
+            return -1;
+        }
         var value = etype.getInputs().get(key);
         if(value == null) {
             return -1;
         }
         ItemType itemType = ConfigManager.getItemType(value);
+        if (itemType == null) {
+            return -1;
+        }
         int[] ints = script2546(itemType);
         return ints[0];
     }
@@ -231,83 +238,87 @@ public final class Smithing {
         int int3 = 0;
         int int4 = 0;
 
-        StructType struct0 = ConfigManager.getStructType(structId);
+        StructType struct = ConfigManager.getStructType(structId);
+        if (struct == null) {
+            ScriptConsole.println("[PublixAPI - Smithing] Script2615: struct is null");
+            return new int[]{0, 0};
+        }
 
         switch (int1) {
             case 1 -> {
-                obj2 = (int)(struct0.getParams().get(2655));
-                int3 = (int)(struct0.getParams().get(5456));
-                int4 = (int)(struct0.getParams().get(2665));
+                obj2 = struct.getParams().containsKey(2655) ? (int)(struct.getParams().get(2655)) : 0;
+                int3 = struct.getParams().containsKey(5456) ? (int)(struct.getParams().get(5456)) : 0;
+                int4 = struct.getParams().containsKey(2665) ? (int)(struct.getParams().get(2665)) : 0;
             }
             case 2 -> {
-                obj2 = (int)(struct0.getParams().get(2656));
-                int3 = (int)(struct0.getParams().get(5457));
-                int4 = (int)(struct0.getParams().get(2666));
+                obj2 = struct.getParams().containsKey(2656) ? (int)(struct.getParams().get(2656)) : 0;
+                int3 = struct.getParams().containsKey(5457) ? (int)(struct.getParams().get(5457)) : 0;
+                int4 = struct.getParams().containsKey(2666) ? (int)(struct.getParams().get(2666)) : 0;
             }
             case 3 -> {
-                obj2 = (int)(struct0.getParams().get(2657));
-                int3 = (int)(struct0.getParams().get(5458));
-                int4 = (int)(struct0.getParams().get(2667));
+                obj2 = struct.getParams().containsKey(2657) ? (int)(struct.getParams().get(2657)) : 0;
+                int3 = struct.getParams().containsKey(5458) ? (int)(struct.getParams().get(5458)) : 0;
+                int4 = struct.getParams().containsKey(2667) ? (int)(struct.getParams().get(2667)) : 0;
             }
             case 4 -> {
-                obj2 = (int)(struct0.getParams().get(2658));
-                int3 = (int)(struct0.getParams().get(5459));
-                int4 = (int)(struct0.getParams().get(2668));
+                obj2 = struct.getParams().containsKey(2658) ? (int)(struct.getParams().get(2658)) : 0;
+                int3 = struct.getParams().containsKey(5459) ? (int)(struct.getParams().get(5459)) : 0;
+                int4 = struct.getParams().containsKey(2668) ? (int)(struct.getParams().get(2668)) : 0;
             }
             case 5 -> {
-                obj2 = (int)(struct0.getParams().get(2659));
-                int3 = (int)(struct0.getParams().get(5460));
-                int4 = (int)(struct0.getParams().get(2669));
+                obj2 = struct.getParams().containsKey(2659) ? (int)(struct.getParams().get(2659)) : 0;
+                int3 = struct.getParams().containsKey(5460) ? (int)(struct.getParams().get(5460)) : 0;
+                int4 = struct.getParams().containsKey(2669) ? (int)(struct.getParams().get(2669)) : 0;
             }
             case 6 -> {
-                obj2 = (int)(struct0.getParams().get(2660));
-                int3 = (int)(struct0.getParams().get(5461));
-                int4 = (int)(struct0.getParams().get(2670));
+                obj2 = struct.getParams().containsKey(2660) ? (int)(struct.getParams().get(2660)) : 0;
+                int3 = struct.getParams().containsKey(5461) ? (int)(struct.getParams().get(5461)) : 0;
+                int4 = struct.getParams().containsKey(2670) ? (int)(struct.getParams().get(2670)) : 0;
             }
             case 7 -> {
-                obj2 = (int)(struct0.getParams().get(2661));
-                int3 = (int)(struct0.getParams().get(5462));
-                int4 = (int)(struct0.getParams().get(2671));
+                obj2 = struct.getParams().containsKey(2661) ? (int)(struct.getParams().get(2661)) : 0;
+                int3 = struct.getParams().containsKey(5462) ? (int)(struct.getParams().get(5462)) : 0;
+                int4 = struct.getParams().containsKey(2671) ? (int)(struct.getParams().get(2671)) : 0;
             }
             case 8 -> {
-                obj2 = (int)(struct0.getParams().get(2662));
-                int3 = (int)(struct0.getParams().get(5463));
-                int4 = (int)(struct0.getParams().get(2672));
+                obj2 = struct.getParams().containsKey(2662) ? (int)(struct.getParams().get(2662)) : 0;
+                int3 = struct.getParams().containsKey(5463) ? (int)(struct.getParams().get(5463)) : 0;
+                int4 = struct.getParams().containsKey(2672) ? (int)(struct.getParams().get(2672)) : 0;
             }
             case 9 -> {
-                obj2 = (int)(struct0.getParams().get(2663));
-                int3 = (int)(struct0.getParams().get(5464));
-                int4 = (int)(struct0.getParams().get(2673));
+                obj2 = struct.getParams().containsKey(2663) ? (int)(struct.getParams().get(2663)) : 0;
+                int3 = struct.getParams().containsKey(5464) ? (int)(struct.getParams().get(5464)) : 0;
+                int4 = struct.getParams().containsKey(2673) ? (int)(struct.getParams().get(2673)) : 0;
             }
             case 10 -> {
-                obj2 = (int)(struct0.getParams().get(2664));
-                int3 = (int)(struct0.getParams().get(5465));
-                int4 = (int)(struct0.getParams().get(2674));
+                obj2 = struct.getParams().containsKey(2664) ? (int)(struct.getParams().get(2664)) : 0;
+                int3 = struct.getParams().containsKey(5465) ? (int)(struct.getParams().get(5465)) : 0;
+                int4 = struct.getParams().containsKey(2674) ? (int)(struct.getParams().get(2674)) : 0;
             }
             case 11 -> {
-                obj2 = (int)(struct0.getParams().get(5451));
-                int3 = (int)(struct0.getParams().get(5466));
-                int4 = (int)(struct0.getParams().get(5471));
+                obj2 = struct.getParams().containsKey(5451) ? (int)(struct.getParams().get(5451)) : 0;
+                int3 = struct.getParams().containsKey(5466) ? (int)(struct.getParams().get(5466)) : 0;
+                int4 = struct.getParams().containsKey(5471) ? (int)(struct.getParams().get(5471)) : 0;
             }
             case 12 -> {
-                obj2 = (int)(struct0.getParams().get(5452));
-                int3 = (int)(struct0.getParams().get(5467));
-                int4 = (int)(struct0.getParams().get(5472));
+                obj2 = struct.getParams().containsKey(5452) ? (int)(struct.getParams().get(5452)) : 0;
+                int3 = struct.getParams().containsKey(5467) ? (int)(struct.getParams().get(5467)) : 0;
+                int4 = struct.getParams().containsKey(5472) ? (int)(struct.getParams().get(5472)) : 0;
             }
             case 13 -> {
-                obj2 = (int)(struct0.getParams().get(5453));
-                int3 = (int)(struct0.getParams().get(5468));
-                int4 = (int)(struct0.getParams().get(5473));
+                obj2 = struct.getParams().containsKey(5453) ? (int)(struct.getParams().get(5453)) : 0;
+                int3 = struct.getParams().containsKey(5468) ? (int)(struct.getParams().get(5468)) : 0;
+                int4 = struct.getParams().containsKey(5473) ? (int)(struct.getParams().get(5473)) : 0;
             }
             case 14 -> {
-                obj2 = (int)(struct0.getParams().get(5454));
-                int3 = (int)(struct0.getParams().get(5469));
-                int4 = (int)(struct0.getParams().get(5474));
+                obj2 = struct.getParams().containsKey(5454) ? (int)(struct.getParams().get(5454)) : 0;
+                int3 = struct.getParams().containsKey(5469) ? (int)(struct.getParams().get(5469)) : 0;
+                int4 = struct.getParams().containsKey(5474) ? (int)(struct.getParams().get(5474)) : 0;
             }
             case 15 -> {
-                obj2 = (int)(struct0.getParams().get(5455));
-                int3 = (int)(struct0.getParams().get(5470));
-                int4 = (int)(struct0.getParams().get(5475));
+                obj2 = struct.getParams().containsKey(5455) ? (int)(struct.getParams().get(5455)) : 0;
+                int3 = struct.getParams().containsKey(5470) ? (int)(struct.getParams().get(5470)) : 0;
+                int4 = struct.getParams().containsKey(5475) ? (int)(struct.getParams().get(5475)) : 0;
             }
         }
 
